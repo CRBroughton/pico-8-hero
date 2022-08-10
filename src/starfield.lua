8c6a2938e9470879ea37866f135b1453eeb2484f
CRBroughton <crbroughton@posteo.uk>
2022-08-10 20:37:28 +0100

    feat(starfield): animated stars

--- src/starfield.lua
+++ src/starfield.lua
@@ -4,3 +4,16 @@ function starfield()
         pset(starx[i], stary[i], 7)
     end
 end
+
+function animatestars()
+    for i = 1, #stary do -- # - shortcut to length
+        local sy = stary[i] -- local scope variable
+
+        sy = sy + starspeed[i]
+
+        if sy > 128 then
+            sy = sy - 128
+        end
+        stary[i] = sy -- sets global variable value to local
+    end
+end
