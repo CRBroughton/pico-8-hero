84384fd9ff67f6c6f8ad5aa07f4cef51ba7f83d2
CRBroughton <crbroughton@posteo.uk>
2022-09-08 21:03:43 +0100

    fix(enemies): :bug: enemies with widths larger than 8 now are taken into account for collision

--- src/utils.lua
+++ src/utils.lua
@@ -5,13 +5,13 @@ end
 function iscolliding(spritea, spriteb)
     local a_left = spritea.x
     local a_top = spritea.y
-    local a_right = spritea.x + 7
-    local a_bottom = spritea.y + 7
+    local a_right = spritea.x + spritea.collisionwidth - 1
+    local a_bottom = spritea.y + spritea.collisionheight - 1
 
     local b_left = spriteb.x
     local b_top = spriteb.y
-    local b_right = spriteb.x + 7
-    local b_bottom = spriteb.y + 7
+    local b_right = spriteb.x + spriteb.collisionwidth - 1
+    local b_bottom = spriteb.y + spriteb.collisionheight - 1
 
     if a_top > b_bottom then return false end
     if b_top > a_bottom then return false end
