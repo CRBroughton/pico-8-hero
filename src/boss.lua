799373ad2ad4635a3034ff0131c8ce6a27e5bf0b
CRBroughton <crbroughton@posteo.uk>
2022-09-19 16:36:03 +0100

    feat(boss): :construction: add movement for phase 2

--- src/boss.lua
+++ src/boss.lua
@@ -23,17 +23,46 @@ function boss1(enemy)
     if enemy.phasebegin + 8 * 30 < gametime then
         enemy.mission = "boss2"
         enemy.phasebegin = gametime
+        enemy.subphase = 1
     end
     move(enemy)
 end
 
 function boss2(enemy)
-    debug = "boss2"
+    -- movement
+    local speed = 2
+    if enemy.subphase == 1 then
+        enemy.sx =- speed
+        if enemy.x <= 3 then
+            enemy.subphase = 2
+        end
+    elseif enemy.subphase == 2 then
+        enemy.sx = 0
+        enemy.sy = speed
 
-    if enemy.phasebegin + 8 * 30 < gametime then
-        enemy.mission = "boss3"
-        enemy.phasebegin = gametime
-    end 
+        if enemy.y >= 96 then
+            enemy.subphase = 3
+        end
+
+    elseif enemy.subphase == 3 then
+        enemy.sx = speed
+        enemy.sy = 0
+
+        if enemy.x >= 93 then
+            enemy.subphase = 4
+        end
+    elseif enemy.subphase == 4 then
+        enemy.sx = 0
+        enemy.sy = - speed
+
+        if enemy.y <= 25 then
+            -- transition
+            enemy.mission = "boss3"
+            enemy.phasebegin = gametime
+        end
+    end
+    -- shooting
+    debug = "boss2"
     move(enemy)
 end
 
@@ -44,7 +73,7 @@ function boss3(enemy)
         enemy.mission = "boss4"
         enemy.phasebegin = gametime
     end 
-    move(enemy)
+    -- move(enemy)
 
 end
 
